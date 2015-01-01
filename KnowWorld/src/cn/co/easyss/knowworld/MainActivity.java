2cff2a74056c3452667e4280386a59faddeaeaf8
liweijp <liweijp@gmail.com>
2015-01-01 11:01:36 +0900

    by li

--- KnowWorld/src/cn/co/easyss/knowworld/MainActivity.java
+++ KnowWorld/src/cn/co/easyss/knowworld/MainActivity.java
@@ -23,27 +23,30 @@ public class MainActivity extends FragmentActivity implements FragmentTabHost.On
         Button button1 = new Button(this);
         //button1.setBackgroundResource(R.drawable.tab_left);
         button1.setText("TAB1");
-        tabSpec1.setIndicator(button1);
+        //tabSpec1.setIndicator(button1);
+        tabSpec1.setIndicator("国内");
         Bundle bundle1 = new Bundle();
-        bundle1.putString("name", "Tab1");
+        bundle1.putString("name", "国内");
         tabHost.addTab(tabSpec1, FragmentTab.class, bundle1);
          
         TabSpec tabSpec2 = tabHost.newTabSpec("tab2");
         Button button2 = new Button(this);
-        button2.setText("TAB2");
+        button2.setText("海外");
        // button2.setBackgroundResource(R.drawable.tab_center);
-        tabSpec2.setIndicator(button2);
+        //tabSpec2.setIndicator(button2);
+        tabSpec2.setIndicator("海外");
         Bundle bundle2 = new Bundle();
-        bundle2.putString("name", "Tab2");
+        bundle2.putString("name", "海外");
         tabHost.addTab(tabSpec2, FragmentTab.class, bundle2);
          
         TabSpec tabSpec3 = tabHost.newTabSpec("tab3");
         Button button3 = new Button(this);
-        button3.setText("TAB3");
+        button3.setText("社会");
         //button3.setBackgroundResource(R.drawable.tab_right);
-        tabSpec3.setIndicator(button3);
+        //tabSpec3.setIndicator(button3);
+        tabSpec3.setIndicator("社会");
         Bundle bundle3 = new Bundle();
-        bundle3.putString("name", "Tab3");
+        bundle3.putString("name", "社会");
         tabHost.addTab(tabSpec3, FragmentTab.class, bundle3);
         
 	  }
